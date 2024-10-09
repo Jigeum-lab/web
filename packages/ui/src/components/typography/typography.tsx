6275b0ec351bb600eaac538e86f024215ab15ad3
Seong Gi Dong <sgd0947@gmail.com>
2024-10-09 14:03:15 +0900

    [JIG-4] 버튼컴포넌트 개발 (#3)
    * feat(button): 버튼 컴포넌트 props 설정
    
    JIG-4
    
    * feat(button): 버튼 컴포넌트 스토리북 추가
    
    JIG-4
    
    * feat(button): 버튼컴포넌트 strong 추가
    
    JIG-4

--- packages/ui/src/components/typography/typography.tsx
+++ packages/ui/src/components/typography/typography.tsx
@@ -1,9 +1,36 @@
 import type React from 'react';
 
-export interface TypographyProps extends React.HTMLAttributes<HTMLSpanElement> {
+import type {
+  TypographyColor,
+  TypographyTextAlign,
+  TypographyTextLevel,
+} from '@/components/typography';
+
+type ElementType = React.ElementType;
+
+type PropsOf<T extends ElementType> = React.ComponentPropsWithoutRef<T>;
+
+export interface TypographyOwnProps<T extends ElementType> {
+  as?: T;
+  level: TypographyTextLevel;
+  textAlign?: TypographyTextAlign;
+  color?: TypographyColor;
+  strong?: boolean;
   children: React.ReactNode;
 }
 
-export const Typography = ({ children, ...rest }: TypographyProps) => {
-  return <span {...rest}>{children}</span>;
+export type TypographyProps<T extends ElementType> = TypographyOwnProps<T> &
+  Omit<PropsOf<T>, keyof TypographyOwnProps<T>>;
+
+export const Typography = <T extends ElementType = 'span'>({
+  as,
+  children,
+  level,
+  textAlign,
+  strong = false,
+  color,
+  ...rest
+}: TypographyProps<T>) => {
+  const Component = as || 'span';
+  return <Component {...rest}>{children}</Component>;
 };
