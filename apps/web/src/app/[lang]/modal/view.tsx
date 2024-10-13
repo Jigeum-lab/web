a2d2a88516bf48deaf57cc298653435d5db8c1bb
Seong Gi Dong <sgd0947@gmail.com>
2024-10-13 23:47:35 +0900

    feat(layout): bottom sheet 라이브러리 추가
    JIG-26

--- apps/web/src/app/[lang]/modal/view.tsx
+++ apps/web/src/app/[lang]/modal/view.tsx
@@ -1,10 +1,13 @@
 'use client';
 import { useOverlay } from '@toss/use-overlay';
+import { useState } from 'react';
+import { Sheet } from 'react-modal-sheet';
 
 import { Modal } from '@/components/basic/Modal';
 
 export const ModalView = () => {
   const overlay = useOverlay();
+  const [isOpen, setOpen] = useState(false);
 
   const openAlert = () => {
     return overlay.open(({ isOpen, close }) => (
@@ -18,7 +21,25 @@ export const ModalView = () => {
     ));
   };
 
-  return <button onClick={openAlert}>modal</button>;
+  return (
+    <div>
+      <button onClick={openAlert}>modal</button>
+      <button onClick={() => setOpen(true)}>Open sheet</button>
+
+      <Sheet
+        isOpen={isOpen}
+        onClose={() => setOpen(false)}
+        snapPoints={[-50, 0.3, 100, 0]}
+        initialSnap={1}
+      >
+        <Sheet.Container>
+          <Sheet.Header />
+          <Sheet.Content>{/* Your sheet content goes here */}</Sheet.Content>
+        </Sheet.Container>
+        <Sheet.Backdrop />
+      </Sheet>
+    </div>
+  );
 };
 
 export default ModalView;
