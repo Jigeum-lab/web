bca184db3068acc926f983c7c23d89b1b0b850ea
Seong Gi Dong <sgd0947@gmail.com>
2024-10-21 15:24:20 +0900

    feat(main): imageGallery 추가
    JIG-26

--- apps/web/src/app/[lang]/main/view.tsx
+++ apps/web/src/app/[lang]/main/view.tsx
@@ -1,12 +1,33 @@
 'use client';
 import { useTranslations } from 'next-intl';
 
+import { ImageGallery } from '@/components/basic/ImageGallery';
+
 const MainView = () => {
   const t = useTranslations('home');
   return (
     <div>
       <h1>{t('IndexPage.title')}</h1>
-      <div>Image Banner</div>
+      <div>
+        Image Banner
+        <ImageGallery
+          isModal={true}
+          images={[
+            {
+              original: 'https://picsum.photos/id/1018/1000/600/',
+              thumbnail: 'https://picsum.photos/id/1018/250/150/',
+            },
+            {
+              original: 'https://picsum.photos/id/1015/1000/600/',
+              thumbnail: 'https://picsum.photos/id/1015/250/150/',
+            },
+            {
+              original: 'https://picsum.photos/id/1019/1000/600/',
+              thumbnail: 'https://picsum.photos/id/1019/250/150/',
+            },
+          ]}
+        />
+      </div>
     </div>
   );
 };
